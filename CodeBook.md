a2e5db6320bd52c85078d721c430156236969c4c
davidberkowitz <davidberkowitz2009@gmail.com>
2014-11-22 16:06:51 -0800

    Close to done

--- CodeBook.md
+++ CodeBook.md
@@ -1,15 +1,108 @@
+---
+output: pdf_document
+---
 # Getting-and-Cleaning-Data-Week-3
 
 ### Code Book
 
 #### Intro
 
+As dicussed in README.md [link pending], the assignment is: 
+
+> You should create one R script called run_analysis.R that does the following. 
+>
+> 1. Merges the training and the test sets to create one data set.
+>
+> 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+>
+> 3. Uses descriptive activity names to name the activities in the data set
+>
+> 4. Appropriately labels the data set with descriptive variable names. 
+>
+> 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+We are given a zip file which expands into this hierarchy of files:
+Davids-MacBook-Air:Getting-And-Cleaning-Data-Week-3 davidberkowitz$ ls -lR UCI\ HAR\ Dataset/
+total 64
+-rwxr-xr-x@ 1 davidberkowitz  staff   4453 Dec 10  2012 README.txt
+-rwxr-xr-x@ 1 davidberkowitz  staff     80 Oct 10  2012 activity_labels.txt
+-rwxr-xr-x@ 1 davidberkowitz  staff  15785 Oct 11  2012 features.txt
+-rwxr-xr-x@ 1 davidberkowitz  staff   2809 Oct 15  2012 features_info.txt
+drwxr-xr-x@ 6 davidberkowitz  staff    204 Nov 29  2012 test
+drwxr-xr-x@ 6 davidberkowitz  staff    204 Nov 29  2012 train
+
+UCI HAR Dataset//test:
+total 51712
+drwxr-xr-x@ 11 davidberkowitz  staff       374 Nov 29  2012 Inertial Signals
+-rwxr-xr-x@  1 davidberkowitz  staff  26458166 Nov 29  2012 X_test.txt
+-rwxr-xr-x@  1 davidberkowitz  staff      7934 Nov 29  2012 subject_test.txt
+-rwxr-xr-x@  1 davidberkowitz  staff      5894 Nov 29  2012 y_test.txt
+
+UCI HAR Dataset//train:
+total 128992
+drwxr-xr-x@ 11 davidberkowitz  staff       374 Nov 29  2012 Inertial Signals
+-rwxr-xr-x@  1 davidberkowitz  staff  66006256 Nov 29  2012 X_train.txt
+-rwxr-xr-x@  1 davidberkowitz  staff     20152 Nov 29  2012 subject_train.txt
+-rwxr-xr-x@  1 davidberkowitz  staff     14704 Nov 29  2012 y_train.txt
+
 #### Step 1
+
+> 1. Merges the training and the test sets to create one data set.
+
+The data sets to be merged are:
+./UCI HAR Dataset/test/y_test.txt
+./UCI HAR Dataset/train/y_train.txt
+
+We will assume the dataset has been unzipped and lives in the current directory as directory hierarchy starting at "UCI HAR Dataset".
+
+The intermediate result is saved in the file:
+step1.rds
 
 #### Step 2
 
+> 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+
+The current merged data set has no column labels. We now need them to determine which are mean and standard deviation or std.
+
+The column labels are contained in the file:
+./UCI HAR Dataset/features.txt
+
+A quick grep of mean() and std() reveals there are unwanted columns of the form *meanFreq* which are removed in a second step.
+
+The intermediate result is saved in the file:
+step2.rds
+
 #### Step 3
+
+> 3. Uses descriptive activity names to name the activities in the data set
+
+This is a straightforward replacement of a numeric value in one column from domain  [1:6] to a character string held in the following file:
+
+At the same time the activity column is given the name: "activity".
+
+The intermediate result is saved in the file:
+step3.rds
 
 #### Step 4
 
+> 4. Appropriately labels the data set with descriptive variable names. 
+
+For the most part this has already been done in steps 2. The question is: is this "descriptive" enough? For example is it meaningful enough that variables of the form t<something> are time-domain while those of the form f<something> are frequency-domain? Explaining Fourier transforms seems beyond the scope of this project and the reader should consult other readily available sources findable with Google for example.
+
+For the moment I have decided that the variable names are sufficiently descriptive. If I have sufficient time and interest I might make some small improvements. In any case you are directed to the following file which does quite a good job of decribing the data columns:
+./UCI HAR Dataset/features_info.txt
+
+The intermediate result is saved in the file:
+step4.rds
+
 #### Step 5
+
+> 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+This is perhaps the most complex data transformation in this project. There are 30 subjects and 6 activities. Which means there will be at most 180 rows in this final result (if all activities were captured for all subjects).
+
+The final result is saved in the file:
+step5.rds
+as well as an exported text file:
+step5.txt
+
--- run_analysis.R
+++ run_analysis.R
@@ -6,7 +6,12 @@ run_analysis <- function ()
 {
 	cat ("run_analysis()\n")
   
-  # Step 1
+	########
+	#
+	# Step 1
+	#
+	########
+  
   # Merge the training and the test sets to create one data set.
 	cat ("Step 1\n")
   
@@ -14,118 +19,177 @@ run_analysis <- function ()
   # In each directory there are 3 files of interest.
   # Assumes we are one level above UCI HAR Dataset
 
-  cat ("Reading subject_test:\n")
-  subject_test = read.table ("UCI HAR Dataset/test/subject_test.txt")
-  str (subject_test)
+  cat ("Reading X_test:\n")
+  X_test = read.table ("UCI HAR Dataset/test/X_test.txt")
+  str (X_test)
+  
+  # now do the same for the training data
+
+	cat ("Reading X_train:\n")
+	X_train = read.table ("UCI HAR Dataset/train/X_train.txt")
+	str (X_train)
+  
+  # finally, rbind the test data with the training data
+  
+  cat ("new_data:")
+  new_data = rbind (X_test, X_train)
+  str (new_data)
+  
+  saveRDS (new_data, file="step1.rds")
+  
+	########
+	#
+	# Step 2
+	#
+	########
+  
+	# Extract only the measurements on the mean and standard deviation for each measurement.   
+	cat ("Step 2\n")
+  
+  # read in the feature names - they are in the second column
+  
+  cat ("features:")
+	features = read.table ("UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)
+  str (features)
+  
+  cat ("new_data with feature column names")
+	names (new_data) = features[,2]
+  str (new_data)
+  
+  # grab all the columns with std or mean in the name
+  
+	new_data = new_data[,grep ("mean|std", names (new_data))]
+  cat ("new_data with only mean|std:")
+  str (new_data)
+  
+  # new_data has a few too many columns
+  # specifically it includes columns with names like "meanFreq"
+  #
+  # I choose to interpret the instructions to mean:
+  # produce matched columns of data where there is both a mean and a std
+  # and so the "meanFreq" columns need to go
+  #
+  # note use of -grep to exclude columns
+  
+	new_data = new_data [,-grep ("meanFreq", names (new_data))]
+	cat ("new_data without meanFreq:")
+	str (new_data)
+  
+	saveRDS (new_data, file="step2.rds")
+  
+  ########
+  #
+	# Step 3
+  #
+  ########
 	
+  # Uses descriptive activity names to name the activities in the data set
+	cat ("Step 3\n")
+  
+  # grab the activity info
+  
 	cat ("Reading y_test:\n")
 	y_test = read.table ("UCI HAR Dataset/test/y_test.txt")
 	str (y_test)
   
-  cat ("Reading X_test:\n")
-  X_test = read.table ("UCI HAR Dataset/test/X_test.txt")
-  str (X_test)
-
-  # cbind these columns of data together
-  
-  cat ("test_data:")
-  test_data = cbind (subject_test, y_test, X_test)
-  str (test_data)
-  head (test_data)
-  
-  # now do the same for the training data
-  
-	cat ("Reading subject_train:\n")
-	subject_train = read.table ("UCI HAR Dataset/train/subject_train.txt")
-	str (subject_train)
-	
 	cat ("Reading y_train:\n")
 	y_train = read.table ("UCI HAR Dataset/train/y_train.txt")
 	str (y_train)
   
-	cat ("Reading X_train:\n")
-	X_train = read.table ("UCI HAR Dataset/train/X_train.txt")
-	str (X_train)
-	  
-	# cbind these columns of data together
+	cat ("activity_data:")
+	activity_data = rbind (y_test, y_train)
+	str (activity_data)
   
-  cat ("train_data:")
-	train_data = cbind (subject_train, y_train, X_train)
-	str (train_data)
-	head (train_data)
+	# cbind the columns of data together
+	
+	cat ("new_data:")
+	new_data = cbind (activity_data, new_data)
+	str (new_data)
   
-  # finally, rbind the test data with the training data
+  # give the new column a name
   
-  cat ("merged_data:")
-  merged_data = rbind (test_data, train_data)
-  str (merged_data)
-  head (merged_data)
+  names (new_data)[1] = "activity"
+  cat ("new_data with new column name:")
+  str (new_data)
   
-  # give the table some useful column names
-  
-	features = read.table ("UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)
-  
-	# the order of the column names is arbitrary
-  # and depends only on the order in which the columns were bound
-  column_names = c("subject", "activity", features[,2])
-	names (merged_data) = column_names
-  cat ("new names:")
-  str (merged_data)
-  
-  saveRDS (merged_data, file="step1.rds")
-  
-  # Step 2
-	# Extract only the measurements on the mean and standard deviation for each measurement.   
-	cat ("Step 2\n")
-  
-  # grab all the columns with std or mean in the name
-  
-	meanORstd = merged_data[,grep ("mean|std", names (merged_data))]
-  cat ("meanORstd:")
-  names (meanORstd)
-  
-  # this new data frame has a few too many columns
-  # specifically it includes columns with names like "meanFreq"
-  
-  # sadly it also excludes subject and activity
-  
-  # I choose to interpret the instructions to mean:
-  # produce matched columns of data where there is both a mean and a std
-  # and so the "meanFreq" columns need to go
-  # note use of -grep to exclude columns
-  
-	meanORstd = meanORstd [,-grep ("meanFreq", names (meanORstd))]
-	cat ("(updated) meanORstd:")
-	names (meanORstd)
-  
-	saveRDS (meanORstd, file="step2.rds")
-  
-	# Step 3
-	# Uses descriptive activity names to name the activities in the data set
-	cat ("Step 3\n")
+  # read in the activity strings
   
 	activities = read.table ("UCI HAR Dataset/activity_labels.txt", stringsAsFactors = FALSE)
+  cat ("activities:")
+  str (activities)
   
-  # oops - activities is no longer in the data!
-  # if it were, this would replace the activity numbers with strings
+  # replace the activity numbers with strings
   
-	meanORstd$activity = factor (meanORstd$activity, levels=c(1, 2, 3, 4, 5, 6), labels=activities[,2])
+  new_data$activity = factor (new_data$activity, levels=c(1, 2, 3, 4, 5, 6), labels=activities[,2])
   
-	saveRDS (meanORstd, file="step3.rds")
+	saveRDS (new_data, file="step3.rds")
   
+	########
+	#
 	# Step 4
+	#
+	########
+  
   # Appropriately labels the data set with descriptive variable names. 
 	cat ("Step 4\n")
   
   # I feel this has already been done by using features.txt in step 1
+  # However we haven't read in the subject data yet
+  # So do this now
   
+	cat ("Reading subject_train:\n")
+	subject_train = read.table ("UCI HAR Dataset/train/subject_train.txt")
+	str (subject_train)
+  
+	cat ("Reading subject_test:\n")
+	subject_test = read.table ("UCI HAR Dataset/test/subject_test.txt")
+	str (subject_test)
+  
+	cat ("subject_data:")
+	subject_data = rbind (subject_test, subject_train)
+	str (subject_data)
+  
+	# cbind the columns of data together
+	
+	cat ("new_data:")
+	new_data = cbind (subject_data, new_data)
+	str (new_data)
+  
+	# give the new column a name
+	
+	names (new_data)[1] = "subject"
+	cat ("new_data with new column name:")
+	str (new_data)
+  
+	saveRDS (new_data, file="step4.rds")
+  
+	########
+	#
 	# Step 5
+	#
+	########
+  
 	# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 	cat ("Step 5\n")
-
-  #  exit
+  
+  library (ddplyr)
+  
+	#new_data = ddply(new_data, .(activity, subject), colwise(mean))
+	group_by (new_data, activity, subject) %>% summarise_each(funs(mean)) %>% step5_data
+  
+	saveRDS (step5_data, file="step5.rds")
+  
+  # the new data set is independent in that it has been saved in a separate "step" RDS file
+  # ste4.RDS and step5.RDS can be read independently
+  #
+  # (of course this is true for EVERY step in this R file)
+  # all intermediate results are saved
+  
+  # we are done
+  
+  write.table (step5_data, "step5.txt", row.labels=FALSE)
   
   cat ("done\n")
   
-  return (meanORstd)
+  return (new_data)
 }
